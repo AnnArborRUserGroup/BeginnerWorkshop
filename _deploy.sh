c6e4fbc9b215ab542c2296fa8f331e52f65fd935
Ellis Valentiner <ellisvalentiner@gmail.com>
2016-12-11 17:24:31 -0500

    Fix the travis repo slug

--- _deploy.sh
+++ _deploy.sh
@@ -8,7 +8,7 @@ set -e
 git config --global user.email "ellisvalentiner@gmail.com"
 git config --global user.name "Ellis Valentiner"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/AnnArborRUserGroup/BeginnerWorkshop.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
